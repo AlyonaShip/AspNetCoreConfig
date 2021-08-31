bbbb357eb32aebfbe602c09b837a98f1dd862cea
AlyonaShip <AlyonaShip>
2021-08-31 15:13:44 +0300

    added column city

--- DataAccessLayer/Entities/User.cs
+++ DataAccessLayer/Entities/User.cs
@@ -11,5 +11,7 @@ namespace DataAccessLayer.Entities
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string City { get; set; }
+
     }
 }
